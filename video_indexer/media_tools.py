202dbdb6de1d5ca5e9e23df25cc3110dbf6ea915
Colin Bitterfield <colin@bitterfield.com>
2019-12-21 14:50:52 -0800

    Update push

---
+++ video_indexer/media_tools.py
@@ -0,0 +1,17 @@
+'''
+Created on Dec 21, 2019
+
+@author: colinbitterfield
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
